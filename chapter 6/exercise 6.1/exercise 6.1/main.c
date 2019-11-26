553a61744d8c9659c2a5a63f2f090103c91d7c6e
rakhi-dhanfule <rakhidhanfule@gmail.com>
2019-11-26 12:24:01 +0530

    exercise 6.1

---
+++ chapter 6/exercise 6.1/exercise 6.1/main.c
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: ANURADHA
+ *
+ * Created on November 26, 2019, 12:01 PM
+ */
+
+#include<stdio.h>
+main()
+{
+ 
+int number, div, ifprime;
+ 
+ 
+ 
+for (number=2;number<=300;number++)
+ 
+{
+ 
+for (div=2; div<number; div++)
+ 
+{
+if (number%div==0)
+{
+ 
+ifprime=0;
+break;
+}
+ 
+ifprime=1;
+ 
+}
+ 
+if (ifprime)
+{
+printf("\n%d", number);
+ 
+}
+}
+}
